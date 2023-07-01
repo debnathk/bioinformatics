890cb133f113fe931dc2d3965c60de534d64a603
Kusal <debnathk1997@gmail.com>
2023-07-01 17:16:53 +0530

    Add file

---
+++ R/control_structures/if_else.r
@@ -0,0 +1,23 @@
+# simple if-else
+x <- 10
+
+if (x > 5){
+  print("x is greater than 5")
+}else{
+  print("x is less than 5")
+}
+
+# output: [1] "x is greater than 5"
+
+# nested if-else
+y <- 15
+
+if (y < 5){
+  print("y is less than 5")
+}else if (y > 20){
+  print("y is greater than 20")
+}else {
+  print("y is equal to 15")
+}
+
+# output: [1] "y is equal to 15"
