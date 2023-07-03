551ed1e8e178a014032e790466d8f1c0a00da8d5
Kusal <debnathk1997@gmail.com>
2023-07-03 20:32:08 +0530

    Add, Modify file

---
+++ R/file_handling/intermediate_handling.r
@@ -0,0 +1,13 @@
+# write a file
+data <- data.frame(
+  Name = c("John", "Alice", "Bob"),
+  Age = c(25, 30, 28),
+  Salary = c(45000, 55000, 50000)
+)
+file_path <- "intermediate_file.csv"
+write.table(data, file_path, sep = ",", col.names = TRUE, row.names = FALSE)
+
+# read a file
+file_path <- "intermediate_file.csv"
+read_data <- read.table(file_path, header = TRUE, sep = ",")
+print(read_data)
