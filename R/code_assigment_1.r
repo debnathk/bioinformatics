fe844d5c71d989c552e022613b9bcd5155356c74
Kusal <debnathk1997@gmail.com>
2023-07-09 13:48:02 +0530

    Add file

---
+++ R/code_assigment_1.r
@@ -0,0 +1,36 @@
+## Question 1: ####
+#   Write an R code to calculate the sum of all numbers from 1 to 10 and store the
+#   result in a variable called `sum_result`.
+# Solution
+
+# Question 2: ####
+#   Write a function in R called `multiply_by_two` that takes a numeric vector as
+#   input and returns a new vector with each element multiplied by 2. For example,
+#     if the input vector is `c(1, 2, 3, 4)`, the function should return `c(2, 4, 6, 8)`.
+
+# Question 3: ####
+#   Create a data frame in R called `student_scores` with the following data:
+#   ```
+# Name   Score
+# John   85
+# Emily  92
+# Daniel 78
+# Sophia 88
+# ```
+# Write R code to add a new column called `Grade` to the `student_scores` data frame,
+# which assigns grades 'A', 'B', 'C', or 'D' based on the following criteria:
+#   - Score >= 90: 'A'
+# - Score >= 80: 'B'
+# - Score >= 70: 'C'
+# - Otherwise: 'D'
+
+# Question 4: ####
+#   Write an R code to generate a histogram of 100 random numbers following a normal
+#   distribution with a mean of 0 and standard deviation of 1. Set the title of the
+#   plot as "Random Numbers Histogram".
+
+# Question 5: ####
+#   Create a function in R called `calculate_fibonacci` that takes an integer `n`
+#   as input and returns a vector of the first `n` Fibonacci numbers. The Fibonacci
+#   sequence starts with 0 and 1, and each subsequent number is the sum of the two
+#   preceding numbers. For example, if `n = 6`, the function should return `c(0, 1, 1, 2, 3, 5)`.
