c88bbf0607d97dbc67ebb6de039994d2cf9543e8
Kusal <debnathk1997@gmail.com>
2023-07-22 13:34:53 +0530

    First commit blast.r

---
+++ R/sequence_alignment_and_analysis/blast.r
@@ -0,0 +1,5 @@
+# Goals - 
+#   1. Implementing BLAST queries using rBLAST package
+#   2. Build a small reference database of the V. cholerae genome
+#   3. Search for BLASt hits maching to ctxAB gene (produces cholera toxin) region
+#   4. Illustrate a generalized BLAST workflow
