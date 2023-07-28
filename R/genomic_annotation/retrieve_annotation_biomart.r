7311a90b849d6a28c54e037b4d91f9ee001adce8
Kusal <debnathk1997@gmail.com>
2023-07-28 22:33:43 +0530

    Modify file

--- R/genomic_annotation/retrieve_annotation_biomart.r
+++ R/genomic_annotation/retrieve_annotation_biomart.r
@@ -1,2 +1,41 @@
 # Retrieving gene and genome annotation form BioMart
 
+# Load packages ####
+library(biomaRt)
+
+# List marts in the selected example database - gramene:
+listMarts(host = "https://ensembl.gramene.org")
+
+# Create a connection to the selected mart:
+gramene_connection <- useMart(biomart = "ENSEMBL_MART_PLANT", 
+        host = "https://ensembl.gramene.org")
+gramene_connection
+
+# List datasets in the mart:
+data_sets <- listDatasets(gramene_connection)
+data_sets
+
+data_set_connection <- useMart("atrichopoda_eg_gene",
+                               biomart = "ENSEMBL_MART_PLANT", 
+                               host = "https://ensembl.gramene.org")
+data_set_connection
+
+# List the datatypes to retrieve ####
+attributes <- listAttributes(data_set_connection)
+head(attributes)
+
+# Get a vector of all chromosome names ####
+chrom_names <- getBM(attributes = c("chromosome_name"),
+      mart = data_set_connection)
+head(chrom_names)
+
+# Create filters to query data ####
+filters <- listFilters(data_set_connection)
+head(filters)
+
+# Get gene IDs on the first chromosome ####
+first_chr <- chrom_names$chromosome_name[1]
+genes <- getBM(attributes = c("ensembl_gene_id", "description"),
+               filters = c("chromosome_name"), values = c(first_chr),
+               mart = data_set_connection)
+head(genes)
