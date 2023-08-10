66e9510a5ac9c827800593d7eb06d35272019f75
Kusal <debnathk1997@gmail.com>
2023-08-10 20:47:44 +0530

    Modify file

--- R/statistical_model+ml/generating_simulated_dataset.r
+++ R/statistical_model+ml/generating_simulated_dataset.r
@@ -4,3 +4,11 @@
 set.seed(1)
 rand_unifs_10 <- runif(n = 10, min = 0, max = 1)
 rand_unifs_10
+
+## Visualizing a uniform distribution
+rand_unifs_1000 <- runif(n = 1000, min = 0, max = 1)
+hist(rand_unifs_1000,
+     xlab = "Random value (X)",
+     main = "",
+     cex.lab = 1.5,
+     cex.axis = 1.5)
