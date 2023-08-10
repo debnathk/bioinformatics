b767e91b3874e29f21c1de21e712796b695ce3fb
Kusal <debnathk1997@gmail.com>
2023-08-10 08:47:05 +0530

    Modify file

--- R/statistical_model+ml/generating_simulated_dataset.r
+++ R/statistical_model+ml/generating_simulated_dataset.r
@@ -1,1 +1,6 @@
-# Generating a simulated dataset to represent a background
+# Generating a simulated dataset to represent a background ####
+
+## Sampling from a uniform distribution ####
+set.seed(1)
+rand_unifs_10 <- runif(n = 10, min = 0, max = 1)
+rand_unifs_10
